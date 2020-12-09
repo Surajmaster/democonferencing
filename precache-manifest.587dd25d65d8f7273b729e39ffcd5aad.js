a44dc55fdb52faaf89e5d08666e215f2ffcb9952
Suraj Yadav <glorioussuraj@gmail.com>
2020-12-09 23:37:44 +0530

    Updates

---
+++ precache-manifest.587dd25d65d8f7273b729e39ffcd5aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c62049cc773e404223be5c9eeb2084",
+    "url": "/democonferencing/index.html"
+  },
+  {
+    "revision": "119b4f2deccc49ad4a58",
+    "url": "/democonferencing/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d33ac65d058819ea06c9",
+    "url": "/democonferencing/static/css/main.779e5a27.chunk.css"
+  },
+  {
+    "revision": "119b4f2deccc49ad4a58",
+    "url": "/democonferencing/static/js/2.e8e527da.chunk.js"
+  },
+  {
+    "revision": "6bab9387ccbf82effa4ed7e3cb49333f",
+    "url": "/democonferencing/static/js/2.e8e527da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33ac65d058819ea06c9",
+    "url": "/democonferencing/static/js/main.0020b2d3.chunk.js"
+  },
+  {
+    "revision": "3ae36afc809e96aa9f0e",
+    "url": "/democonferencing/static/js/runtime-main.27583814.js"
+  }
+]);
